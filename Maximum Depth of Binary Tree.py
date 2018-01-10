0705aede77da5645b21f9a099276a82169c23208
luguodong910208 <31837352+luguodong910208@users.noreply.github.com>
2018-01-10 21:26:32 +0800

    Update Maximum Depth of Binary Tree.py

--- Maximum Depth of Binary Tree.py
+++ Maximum Depth of Binary Tree.py
@@ -1,8 +1,9 @@
 """Given a binary tree, find its maximum depth.
 
-The maximum depth is the number of nodes along the 
-longest path from the root node down to the farthest 
-leaf node.
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+"""
+"""
+可以采用递归方式来解决，二叉树的最大深度要么在左子树要么在右子树，只要不断地在两个子树中寻找就可以
 """
 # Definition for a binary tree node.
 # class TreeNode:
